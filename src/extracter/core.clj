1058c6bd73b38df6279f43db1350fb280e8524af
Justin Holguin <justin.holguin@puppetlabs.com>
2014-06-30 17:11:58 -0700

    Still working on files with multiple fact docs
    Try parsing blockdevices.rb.

--- src/extracter/core.clj
+++ src/extracter/core.clj
@@ -1,11 +1,16 @@
 (ns extracter.core
+  (:use [clojure.pprint])
   (:require [instaparse.core :as insta]
             [clojure.java.io :as io]))
 
-(def parse (insta/parser (clojure.java.io/resource "doc.bnf")))
+(def parse (insta/parser (io/resource "doc.bnf")))
 
-(def sec (insta/parser
-          "S = Section
-           Section = <#\"# \"> Heading <#\":\">
-           Heading = #\"\w+\"
-           "))
+(defn parse-resource
+  [^String path]
+  (let [rdr (io/reader (io/resource path))]
+    (parse (slurp rdr))))
+
+(defn retry
+  []
+  (with-redefs [parse (insta/parser (io/resource "doc.bnf"))]
+    (pprint (parse-resource "blockdevices.rb"))))
