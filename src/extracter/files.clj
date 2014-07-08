3ed35cea7f56a597f7ac31e1f0b63aec7551cc2b
Justin Holguin <justin.h.holguin@gmail.com>
2014-07-08 15:30:21 -0700

    Scan for facts more carefully
    I was starting to catch editor swap files and whatnot, so I added a
    check for legit .rb files.

--- src/extracter/files.clj
+++ src/extracter/files.clj
@@ -6,17 +6,25 @@
   [^java.util.regex.Pattern re ^String s]
   (boolean (re-find re s)))
 
-(defn fact?
-  "Returns true if the given file appears to contain at least one fact docstring."
-  [^java.io.File f]
-  (with-open [fin (io/reader f)]
-    (->> fin
-         line-seq
-         (some (partial contains? #"# Fact: ")))))
-
 (defn file?
   [^java.io.File f]
   (.isFile f))
+
+(defn ruby-file?
+  [^java.io.File f]
+  (when (file? f)
+    (let [name (.getName f)
+          suffix (last (re-find #"\.(\w+)$" name))]
+      (= suffix "rb"))))
+
+(defn fact?
+  "Returns true if the given file appears to contain at least one fact docstring."
+  [^java.io.File f]
+  (when (ruby-file? f)
+    (with-open [fin (io/reader f)]
+      (->> fin
+           line-seq
+           (some (partial contains? #"# Fact: "))))))
 
 (defn facts-in-dir
   "Returns a lazy sequence of java.io.File instances for every fact source file in a directory path.
@@ -25,5 +33,4 @@
   (->> path
        io/file
        file-seq
-       (filter file?)
        (filter fact?)))
