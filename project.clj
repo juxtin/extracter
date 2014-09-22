71451142e3830f68da5b9eea454e70c308739be5
Justin Holguin <justin.holguin@puppetlabs.com>
2014-09-22 16:41:26 -0700

    Increase JVM heap size
    This appears to alleviate this annoying OOM error that I've been seeing
    on some (but not all?) systems.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot extracter.core
+  :jvm-opts ["-Xmx1G"]
   :profiles {:uberjar {:aot :all}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
